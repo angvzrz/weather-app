d2038378a0cb7adc22dcabddf05ad8058dca8024
angvzrz <ang_vzrz@live.com.mx>
2023-05-15 14:01:34 -0600

    Add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import withMT from '@material-tailwind/react/utils/withMT';
 module.exports = withMT({
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        wr: {
+          'steel-blue': '#5E88BF',
+          'oxford-blue': '#14548C',
+          'pale-cornflower': '#A3BFD9',
+          'pale-blue': '#DFE9F2',
+          pink: '#F0587C',
+        },
+      },
+    },
   },
   plugins: [],
 });
