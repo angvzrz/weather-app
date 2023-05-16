ff48cefbb7a8022b835388149c81896aace4991a
angvzrz <ang_vzrz@live.com.mx>
2023-05-16 00:51:04 -0600

    Update axios get fetch implementation for handling AxiosError

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 export async function getFetch<Response>(url: string): Promise<Response> {
   try {
-    return (await axios.get(url)).data as Response;
+    const response = await axios.get<Response>(url);
+    return response.data;
   } catch (error) {
     console.error(error);
+    throw error;
   }
-
-  return {} as Response;
 }
